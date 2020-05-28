99c8356dde3a11c5ced0e7b46cbfebc523dde14a
afarhansib <afarhansib@gmail.com>
2020-05-28 15:11:47 +0700

    coba ganti main.js lagi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./../../sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(reg => {
         console.log('Service worker registered! 😎', reg)
       })
